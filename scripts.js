0bbfcd9d3f76d54d6a0d74aa2a00ebadd92d88ee
Ceyhun Emre Top <202213709023@ogr.balikesir.edu.tr>
2023-11-16 11:25:32 +0300

    content içerikleri oluşturuldu.

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function showDescription(descriptionId) {
+    var description = document.getElementById(descriptionId);
+    description.style.display = 'block';
+  }
+  
+  function hideDescription(descriptionId) {
+    var description = document.getElementById(descriptionId);
+    description.style.display = 'none';
+  }
+  
